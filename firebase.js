cefc23fe4f9aa00ecf818e3822d00db27723e332
DiginityS <84835941+BrackyB@users.noreply.github.com>
2023-10-13 18:30:28 +0530

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,17 @@
+import firebase from "firebase";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCzL5c4SuA9y_CcoZfkakVxYSIfyucsTZw",
+  authDomain: "virsocial-data.firebaseapp.com",
+  databaseURL: "https://virsocial-data-default-rtdb.firebaseio.com",
+  projectId: "virsocial-data",
+  storageBucket: "virsocial-data.appspot.com",
+  messagingSenderId: "178059844132",
+  appId: "1:178059844132:web:acd31006571e40c65c16b1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
